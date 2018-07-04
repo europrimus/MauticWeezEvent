f1642e04af6313c98902ed5d1ee276f8b5636bfa
GeoffreyMorel <38344457+GeoffreyMorel@users.noreply.github.com>
2018-07-04 10:27:27 +0200

    Update liste.html.php

--- Views/event/liste.html.php
+++ Views/event/liste.html.php
@@ -14,12 +14,31 @@ $view['slots']->set('headerTitle', $titre);
 
 <!-- un peu de style -->
 <style type="text/css">
+  img {
+    display: inline-block;
+  }
+  content-body {
+    margin-left: 10vw;
+    margin-bottom: 20em;   
+  }
+  h3 {
+    margin-top: 30px;
+  }
   ul {
     list-style-type: none;
   }
 
   li {
-    margin: 10px;
+    margin: 12px;
+    cursor: default;
+  }
+
+  .fa {
+    font-size: 23px;
+  }
+
+  li:hover {
+    color: blue;
   }
 
 </style>
@@ -27,27 +46,42 @@ $view['slots']->set('headerTitle', $titre);
 
 <div class="weezevent-content">
   <?php $view['slots']->output('_content'); ?>
-  <img src="<?php echo $view['assets']->getUrl('plugins/MauticWeezeventBundle/Assets/img/weezevent-logo.png') ?>" /><br>
-  <ul>
-  <?php
-    if( is_array($events) ){
-      $count=0;
-      foreach ( $events as $event ){
-        if( $count <= 10 ){
-           if(empty($event->date->end)){
-            $date=$event->date->start;
-          }else{
-            $date=$event->date->end;
+  <div class="container">
+    <div class="row">
+<!-- affiche logo  -->
+      <div class="col-md-6">
+          <img src="<?php echo $view['assets']->getUrl('plugins/MauticWeezeventBundle/Assets/img/weezevent-logo.png') ?>" />
+      </div>
+
+<!-- afficher les evenements + bouton d ' importation  -->
+      <div class="col-md-6">
+       
+          <ul>
+      <?php
+        if( is_array($events) ){
+          $count=0;
+          foreach ( $events as $event ){
+            if( $count <= 10 ){
+               if(empty($event->date->end)){
+                $date=$event->date->start;
+              }else{
+                $date=$event->date->end;
+              }
+              echo '<li>'. date("d/m/Y",strtotime($date) ) .' |'. $event->name .' || <a href="'.$view['router']->generate('plugin_weezevent_tickets',
+                array('idEvent' => $event->id, 'nomEvent'=> $event->name )).'" ><i class="fa fa-cloud-download" aria-hidden="true"></i></a>
+              </li>';
+              $count++;
+            }
           }
-          echo '<li>'. date("d/m/Y",strtotime($date) ) .' |'. $event->name .' -- <a href="'.$view['router']->generate('plugin_weezevent_tickets',
-            array('idEvent' => $event->id, 'nomEvent'=> $event->name )).'" ><button> Importer les contacts </button></a>
-          </li>';
-          $count++;
+        }else{
+          echo "<li>".$view['translator']->trans('plugin.weezevent.event.error.api')."</li>";
         }
-      }
-    }else{
-      echo "<li>".$view['translator']->trans('plugin.weezevent.event.error.api')."</li>";
-    }
-  ?>
-  </ul>
+      ?>
+      </ul>
+
+      </div>
+    </div>
+  </div>
+
+  
 </div>
